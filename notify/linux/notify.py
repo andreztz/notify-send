4bbfe4d5392a434856b021aca2f4371fe6f573b8
André <andreztz@gmail.com>
2020-02-19 18:50:38 -0300

    Add support for embedding images on the linux platform.

--- notify/linux/notify.py
+++ notify/linux/notify.py
@@ -4,11 +4,14 @@ import gi
 gi.require_version("Notify", "0.7")
 
 from gi.repository import Notify
+from gi.repository import GdkPixbuf
 
 
-def Notification(message, title="", app_name=None):
+def Notification(
+    message, summary="", image="dialog-information", app_name=None
+):
     """ Displays a notification using the Gtk API """
     app_name = app_name or sys.argv[0]
     Notify.init(app_name)
-    n = Notify.Notification.new(title, message)
+    n = Notify.Notification.new(summary, message, image)
     n.show()
